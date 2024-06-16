ec5e481c208d1120940f149814264a7faa1fc46c
mjapa21 <mjapa21@freeuni.edu.ge>
2024-06-16 22:05:43 +0400

    starter project

---
+++ src/main/java/quizWebsite/defaultClass.java
@@ -0,0 +1,9 @@
+package quizWebsite;
+
+public class defaultClass {
+}
+//todo: add your own classes inside 'quizWebsite' package
+
+//run your web server by clicking maven icon in intelij
+//then small icon saying 'execute maven goal'
+//then command mvn tomcat7:run
